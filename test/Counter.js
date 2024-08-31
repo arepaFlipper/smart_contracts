0d1dfa2383f9f9e3a266627dc66ad64534dd2d13
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-08-31 18:36:38 -0500

    🛫 refactor tests to apply DRY

--- test/Counter.js
+++ test/Counter.js
@@ -2,20 +2,24 @@ const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
 describe('Counter', () => {
-  it('sets the initial count', async () => {
+  let counter;
+
+  beforeEach(async () => {
     const Counter = await ethers.getContractFactory('Counter');
-    const counter = await Counter.deploy('My Counter', 1);
-    const count = await counter.count();
-    expect(count).to.equal(1);
-    //Fetch the count
+    counter = await Counter.deploy('My Counter', 1);
   });
 
-  it('sets the initial name', async () => {
-    const Counter = await ethers.getContractFactory('Counter');
-    const counter = await Counter.deploy('My Counter', 1);
-    const name = await counter.name();
-    expect(name).to.equal('My Counter');
-    //Fetch the count
+  describe('Deployment', () => {
+    it('sets the initial count', async () => {
+      const count = await counter.count();
+      expect(count).to.equal(1);
+    });
+
+    it('sets the initial name', async () => {
+      const name = await counter.name();
+      expect(name).to.equal('My Counter');
+    });
+
   });
 
 
