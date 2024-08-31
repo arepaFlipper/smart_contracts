89bf9af97f0d66bba07f858fadc1dca622f83426
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-08-31 18:30:11 -0500

    🕠 write the first test

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: UNLICESNSED
 pragma solidity ^0.8.0;
 
 contract Counter {
---
+++ test/Counter.js
@@ -0,0 +1,14 @@
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
+
+describe('Counter', () => {
+  it('stores the count', async () => {
+    const Counter = await ethers.getContractFactory('Counter');
+    const counter = await Counter.deploy('My Counter', 1);
+    const count = await counter.count();
+    expect(count).to.equal(1);
+    //Fetch the count
+  });
+
+
+});
