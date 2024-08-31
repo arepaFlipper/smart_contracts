b1f0979cb7b3fe706fccbeffcd257a4c537035df
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-08-31 18:32:49 -0500

    🪝 add the initial count & anme

--- test/Counter.js
+++ test/Counter.js
@@ -2,11 +2,19 @@ const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
 describe('Counter', () => {
-  it('stores the count', async () => {
+  it('sets the initial count', async () => {
     const Counter = await ethers.getContractFactory('Counter');
     const counter = await Counter.deploy('My Counter', 1);
     const count = await counter.count();
     expect(count).to.equal(1);
+    //Fetch the count
+  });
+
+  it('sets the initial name', async () => {
+    const Counter = await ethers.getContractFactory('Counter');
+    const counter = await Counter.deploy('My Counter', 1);
+    const name = await counter.name();
+    expect(name).to.equal('My Counter');
     //Fetch the count
   });
 
