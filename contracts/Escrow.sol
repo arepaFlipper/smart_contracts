528d0356ef4035e8c2befb24bc12a2ed5617176d
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-09-04 15:10:03 -0500

    ❄️ add transaction test

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -2,16 +2,25 @@
 
 pragma solidity ^0.8.0;
 
+interface IERC721 {
+  function transferFrom(address _from, address _to, uint256 _id) external;
+}
+
 contract Escrow {
   address public nftAddress;
   uint256 public nftID;
+  address payable public seller;
+  address payable public buyer;
 
-  constructor(address _nftAddress, uint256 _nftID) {
+  constructor(address _nftAddress, uint256 _nftID, address payable _seller, address payable _buyer) {
     nftAddress = _nftAddress;
     nftID = _nftID;
+    seller = _seller;
+    buyer = _buyer;
   }
 
   function finalizeSale() public {
     // NOTE: Transfer ownership of property
+    IERC721(nftAddress).transferFrom(seller, buyer, nftID);
   }
 }
--- test/RealEstate.js
+++ test/RealEstate.js
@@ -8,6 +8,7 @@ describe('RealEstate', () => {
     accounts = await ethers.getSigners();
     deployer = accounts[0];
     seller = deployer;
+    buyer = accounts[1];
 
     // NOTE: Load contracts
     const RealEstate = await ethers.getContractFactory('RealEstate');
@@ -15,7 +16,16 @@ describe('RealEstate', () => {
 
     // NOTE: Deploy contracts
     realEstate = await RealEstate.deploy();
-    escrow = await Escrow.deploy(realEstate.address, nftID);
+    escrow = await Escrow.deploy(
+      realEstate.address,
+      nftID,
+      seller.address,
+      buyer.address
+    );
+
+    // NOTE: Seller approves NFT
+    let transaction = await realEstate.connect(seller).approve(escrow.address, nftID);
+    await transaction.wait();
   });
 
   describe('Deployment', async () => {
@@ -24,4 +34,21 @@ describe('RealEstate', () => {
       expect(owner).to.equal(seller.address)
     });
   });
+
+  describe('Selling real estate', async () => {
+    it('executes a succesful transaction', async () => {
+      // NOTE: Expects seller to be NFT owner before the sale
+      const seller_id = await realEstate.ownerOf(nftID);
+      expect(seller_id).to.equal(seller.address)
+
+      let transaction = await escrow.connect(buyer).finalizeSale();
+      await transaction.wait();
+
+      // NOTE: expects buyer to be nft owner after the sale
+      const buyer_id = await realEstate.ownerOf(nftID);
+      expect(buyer_id).to.not.equal(seller_id);
+      expect(buyer_id).to.equal(buyer.address);
+    });
+  });
+
 })
